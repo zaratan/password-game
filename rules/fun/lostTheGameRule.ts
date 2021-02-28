e65b8ffee5ae8ebcfdff20f95dee287de6be94c5
Denis Zaratan Pasin <zaratan@hey.com>
2021-03-01 08:17:24 +0900

    Small fix

--- rules/fun/lostTheGameRule.ts
+++ rules/fun/lostTheGameRule.ts
@@ -2,7 +2,7 @@ import RuleType from '../RuleType';
 
 const useLostTheGameRule: () => RuleType = () => ({
   valid: false,
-  checkFn: (text: string) => /((J'ai\s*)?perdu)|((I\s*)?lost)/.test(text),
+  checkFn: (text: string) => /((J'ai\s*)?perdu)|((I\s*)?lost)/i.test(text),
   title: 'You just lost "The Game"',
   id: 'lost-the-game',
 });
