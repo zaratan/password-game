fa012ccd6608b0ebed23729e95e6ae157723d101
Bryn Dyment <bryndyment@gmail.com>
2021-03-02 11:31:57 +0900

    fix typos

--- rules/medium/piDecimalRule.ts
+++ rules/medium/piDecimalRule.ts
@@ -3,7 +3,7 @@ import RuleType from '../RuleType';
 const usePiDecimalRule: () => RuleType = () => ({
   valid: false,
   checkFn: (text: string) => text.includes('5') && text.includes('8'),
-  title: 'Must contains the 8th and 11th decimal of π',
+  title: 'Must contain the 8th and 11th decimal of π',
   id: 'pi-8-11',
 });
 
--- rules/standard/lowcaseRule.ts
+++ rules/standard/lowcaseRule.ts
@@ -3,7 +3,7 @@ import RuleType from '../RuleType';
 const useLowcaseRule: () => RuleType = () => ({
   valid: false,
   checkFn: (text: string) => new RegExp('\\p{Ll}', 'u').test(text),
-  title: 'Must contains a lowercase letter',
+  title: 'Must contain a lowercase letter',
   id: 'lowercase-simple',
 });
 
