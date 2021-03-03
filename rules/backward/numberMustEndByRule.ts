e61afdad7af7b998e697614b75df91ea42cf9604
Denis Zaratan Pasin <zaratan@hey.com>
2021-03-04 08:37:25 +0900

    Captcha + NumberEndBy don't work together

--- rules/backward/numberMustEndByRule.ts
+++ rules/backward/numberMustEndByRule.ts
@@ -5,6 +5,7 @@ const useNumberMustEndByRule: () => RuleType = () => ({
   checkFn: (text: string) => !/[1-9](\D|$)/.test(text),
   title: 'Each number group must end with a 0',
   id: 'number-group-0',
+  never: ['captcha'],
 });
 
 export default useNumberMustEndByRule;
